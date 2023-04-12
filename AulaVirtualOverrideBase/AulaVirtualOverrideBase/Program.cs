bad7d29a9ca5a1d5581922559b9b8b96863677ed
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-12 11:52:22 -0300

    Virtual e Override

---
+++ AulaVirtualOverrideBase/AulaVirtualOverrideBase/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AulaVirtualOverrideBase.Entities;
+
+namespace AulaVirtualOverrideBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1002, "Alex", 500.0);
+            Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
+
+            acc1.Withdraw(10.0);
+            acc2.Withdraw(10.0);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+        }
+    }
+}
