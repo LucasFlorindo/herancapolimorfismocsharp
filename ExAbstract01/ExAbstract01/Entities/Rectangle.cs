b98a699643fbcae85c9b9f1d3289b41ac369e4d5
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-12 15:47:34 -0300

    Exercício métodos abstratos

---
+++ ExAbstract01/ExAbstract01/Entities/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ExAbstract01.Entities.Enums;
+
+namespace ExAbstract01.Entities
+{
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+
+        public Rectangle(double width, double height, Color color) : base(color)
+        {
+            Width = width;
+            Height = height;  
+        }
+
+        public override double Area()
+        {
+            return Width * Height;
+        }
+    }
+}
