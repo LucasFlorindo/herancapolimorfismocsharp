e7e76c29e61c2fe8ae0921068493c3c7446fd6f0
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-12 14:46:11 -0300

    Exercício de fix

---
+++ ExPolimorfismo02/ExPolimorfismo02/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using ExPolimorfismo02.Entities;
+
+namespace ExPolimorfismo02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.WriteLine("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{n} data:");
+                Console.WriteLine("Common, used or imported (c/u/i)? ");
+                char type = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (type == 'i')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else if (type == 'u')
+                {
+                    Console.WriteLine("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, date));
+                }
+                else if (type == 'c')
+                {
+                    list.Add(new Product(name, price));
+                }
+                else
+                {
+                    Console.WriteLine("Invalid answer!");
+                }
+
+
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("PRICE TAGS: ");
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p.PriceTag(), CultureInfo.InvariantCulture);
+            }
+
+        }
+    }
+}
+
