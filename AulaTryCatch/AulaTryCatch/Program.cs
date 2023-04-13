af1b75ae107f75b74b08c659202d7c3ae2bba759
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-13 08:48:23 -0300

    Aula try catch

---
+++ AulaTryCatch/AulaTryCatch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            //catch (Exception e)..... sempre colocar o tipo de exceção mais específico possivel
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("ERROR " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("FORMAT ERROR " + e.Message);
+            }
+
+
+        }
+    }
+}
