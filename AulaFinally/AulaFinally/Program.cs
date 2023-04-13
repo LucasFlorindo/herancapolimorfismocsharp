0a05c61fa38e843b33c939956d9a35d13a731918
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-13 08:56:49 -0300

    Aula finally

---
+++ AulaFinally/AulaFinally/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(@"C:\Users\Lucas Rodrigues\temp\herança e polimorfismo\AulaFinally\aulaFinally.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
+        }
+    }
+}
