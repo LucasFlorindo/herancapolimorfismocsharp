7dd9d9acd0ce70f362e2a96f03e397ee71231555
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-13 10:24:31 -0300

    Exercício de fixação de Excetptions try n catch

---
+++ ExfixaçãoException01/ExfixaçãoException01/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExfixaçãoException01.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message): base(message)
+        {
+
+        }
+    }
+}
---
+++ ExfixaçãoException01/ExfixaçãoException01/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+using ExfixaçãoException01.Entities;
+using ExfixaçãoException01.Entities.Exceptions;
+
+namespace ExfixaçãoException01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data: ");
+
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine());
+
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine());
+
+            Account acc = new Account(number, holder, balance, withdrawLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            try
+            {
+                acc.Withdraw(amount);
+                Console.WriteLine("New Balance: "+ acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Withdraw error: "+e.Message);
+            }
+
+
+
+        }
+    }
+}
