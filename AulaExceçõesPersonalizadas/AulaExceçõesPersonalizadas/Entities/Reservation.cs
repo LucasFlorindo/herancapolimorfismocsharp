981f3621c4ba5fdf151f3988e507e8355869abfe
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-13 09:40:55 -0300

    AulaException2

--- AulaExceçõesPersonalizadas/AulaExceçõesPersonalizadas/Entities/Reservation.cs
+++ AulaExceçõesPersonalizadas/AulaExceçõesPersonalizadas/Entities/Reservation.cs
@@ -43,10 +43,21 @@ namespace AulaExceçõesPersonalizadas.Entities
 
         }
 
-        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        public string UpdateDates(DateTime checkIn, DateTime checkOut)
         {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                return "Reservation dates for update must be future dates...";
+            }
+            if (checkOut <= checkIn)
+            {
+                return "Check-out date must be after Check-in date...";
+            }
+
             CheckIn = checkIn;
             CheckOut = checkOut;
+            return null;
         }
     }
 }
